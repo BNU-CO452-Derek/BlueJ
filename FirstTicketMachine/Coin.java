94f7937910565488383a0cd64d9e8dde89b399a3
DerekPeacock <derek.peacock@yahoo.com>
2020-09-16 18:50:29 +0100

    Updated enum comment

--- FirstTicketMachine/Coin.java
+++ FirstTicketMachine/Coin.java
@@ -1,8 +1,10 @@
 
 /**
- * Enumeration class Coin - 
+ * Enumeration class Coin - This simulates coins
+ * of value 10, 20, 100 and 200 pence.
+ * 
  * @author Dr Derek Peacock
- * @version 16/Apr/2016
+ * @version 16/Apr/2016, modified 16/09/2020
  */
 public enum Coin
 {
